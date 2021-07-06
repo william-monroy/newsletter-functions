df4ad77944fb1e2b786379c8b14179806578af54
William Monroy <A00829796@itesm.mx>
2021-07-05 23:47:47 -0500

    Organizing project

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    apiKey: process.env.API_KEY,
+    authDomain: "newsletter-wm.firebaseapp.com",
+    projectId: "newsletter-wm",
+    storageBucket: "newsletter-wm.appspot.com",
+    messagingSenderId: "571626044556",
+    appId: "1:571626044556:web:afa6c3306cb641d1440fa9",
+    measurementId: "G-F7N3MVRXMB"
+};
